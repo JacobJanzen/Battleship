a89f74ca9f5ef9d4a3d5a96f41b27cd65fed9b67
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2020-06-26 17:21:14 -0500

    Completed Gameplay

--- battleship.c
+++ battleship.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
-#define SIZE 10 //size of the square board
+#include <stdlib.h>
 
+#define SIZE 10 //size of the square board
 /*size of each ship*/
 #define C_SIZE 5
 #define B_SIZE 4
@@ -9,9 +10,12 @@
 #define S_SIZE 3
 #define P_SIZE 2
 
+/*the boards*/
 char board1[SIZE][SIZE];
 char board2[SIZE][SIZE];
-bool gameOver = false;
+
+bool gameOver = false; //checks when the game ends
+
 /*declare functions*/
 void printBoard(char[SIZE][SIZE]);
 void printOpponentBoard(char[SIZE][SIZE]);
@@ -21,6 +25,7 @@ void addShip(int, int, char, int, char[SIZE][SIZE], char);
 void playGame(char[SIZE][SIZE], char[SIZE][SIZE]);
 
 int main(void){
+	system("@cls||clear");
 	/*fill board with '.'*/
 	for(int i = 0; i < SIZE; i++)
 		for(int j = 0; j < SIZE; j++){
@@ -31,11 +36,22 @@ int main(void){
 	/*set the boards*/
 	setBoard(board1);
 	setBoard(board2);
-
+	/*the game starts*/
 	do{
 		playGame(board1,board2);
-		playGame(board2,board1);
+		printf("Press ENTER to end turn :\n");
+		getchar();
+		getchar();
+		system("@cls||clear");
+		if(!gameOver){
+			playGame(board2,board1);
+			printf("Press ENTER to end turn :\n");
+			getchar();
+			getchar();
+			system("@cls||clear");
+		}
 	}while(!gameOver);
+
 	return 0;
 }
 
@@ -82,19 +98,22 @@ void printOpponentBoard(char board[SIZE][SIZE]){
 }
 
 void setBoard(char board[SIZE][SIZE]){
-	//set carrier location
-	chooseLocation(board, C_SIZE, 'C');
-	chooseLocation(board, B_SIZE, 'B');
-	chooseLocation(board, D_SIZE, 'D');
-	chooseLocation(board, S_SIZE, 'S');
-	chooseLocation(board, P_SIZE, 'P');
-	printBoard(board);
+	chooseLocation(board, C_SIZE, 'C'); //carrier
+	system("@cls||clear");
+	chooseLocation(board, B_SIZE, 'B'); //battleship
+	system("@cls||clear");
+	chooseLocation(board, D_SIZE, 'D'); //destroyer
+	system("@cls||clear");
+	chooseLocation(board, S_SIZE, 'S'); //submarine
+	system("@cls||clear");
+	chooseLocation(board, P_SIZE, 'P'); //patrol boat
+	system("@cls||clear");
 }
 
 void chooseLocation(char board[SIZE][SIZE], int size, char shipNum){
-	int vPos = 10, hPos = 10;
-	int width, height;
-	char hPosChar, direction;
+	int vPos = 10, hPos = 10; //the coordinates
+	int width, height; //the size of the boat
+	char hPosChar, direction; //input variable for x-coord and direction
 	
 	do{
 		printBoard(board); //draw the board to the screen
@@ -118,6 +137,8 @@ void chooseLocation(char board[SIZE][SIZE], int size, char shipNum){
 			default:
 				printf("Enter a valid direction\n");
 		}
+
+		//checks if there is a ship already in the way
 		for(int i = 0; i < width; i++){
 			for(int j = 0; j < height; j++){
 				if(board[hPos+i][vPos+j] != '.'){
@@ -130,15 +151,15 @@ void chooseLocation(char board[SIZE][SIZE], int size, char shipNum){
 			printf("Enter a valid location\n");
 	}while(vPos+height > SIZE || hPos+width > SIZE || (direction != 'h' && direction != 'v') || hPos < 0 || vPos < 0);
 
-	addShip(vPos, hPos, direction, size, board, shipNum);
+	addShip(vPos, hPos, direction, size, board, shipNum); //places the ship on the board
 }
 
 void addShip(int vPos, int hPos, char direction, int size, char board[SIZE][SIZE], char shipNum){
-	if(direction=='h'){
+	if(direction=='h'){//horizontal
 		for(int i = 0; i < size; i++){
 			board[hPos+i][vPos] = shipNum;
 		}
-	}else{
+	}else{//vertical
 		for(int i = 0; i < size; i++){
 			board[hPos][vPos+i] = shipNum;
 		}
@@ -146,12 +167,14 @@ void addShip(int vPos, int hPos, char direction, int size, char board[SIZE][SIZE
 }
 
 void playGame(char pBoard[SIZE][SIZE], char oBoard[SIZE][SIZE]){
-	int hPos, vPos;
-	char hPosChar, temp;
-	bool sink = true, allSank = true;
+	int hPos, vPos;//coordinates
+	char hPosChar, temp;//input variable for x-coord, the type of ship hit
+	bool sink = true, allSank = true;//check whether ship was sank and whether all ships have sank
 	printOpponentBoard(oBoard);
 	printf("\n");
 	printBoard(pBoard);
+
+	/* get the location to aim */
 	do{
 		printf("Enter the location you want to hit (e.g. 7b): ");
 		scanf("%d%c", &vPos, &hPosChar);
@@ -161,16 +184,18 @@ void playGame(char pBoard[SIZE][SIZE], char oBoard[SIZE][SIZE]){
 			printf("Enter a valid Location\n");
 	}while(hPos >= SIZE || vPos >= SIZE || oBoard[hPos][vPos] == 'X' || oBoard[hPos][vPos] == 'O' || hPos < 0 || vPos < 0);
 
-	if(oBoard[hPos][vPos] == '.'){
+	if(oBoard[hPos][vPos] == '.'){ //MISS
 		oBoard[hPos][vPos] = 'O';
 		printf("MISS!\n");
 		allSank = false;
-	}else{
+	}else{ //HIT
 		temp = oBoard[hPos][vPos];
 		oBoard[hPos][vPos] = 'X';
+
+		/* checks if the ship sank and if all ships have sank */
 		for(int i = 0; i < SIZE; i++){
 			for(int j = 0; j < SIZE; j++)
-				if(oBoard[j][i] == temp){
+				if(oBoard[j][i] == temp){//sink
 					sink = false;
 					break;
 				}else if(oBoard[j][i] != '.' && oBoard[j][i] != 'X' && oBoard[j][i] != 'O')
@@ -186,6 +211,6 @@ void playGame(char pBoard[SIZE][SIZE], char oBoard[SIZE][SIZE]){
 		}
 	}
 
-	if(allSank)
+	if(allSank)//ends the game
 		gameOver = true;
 }
